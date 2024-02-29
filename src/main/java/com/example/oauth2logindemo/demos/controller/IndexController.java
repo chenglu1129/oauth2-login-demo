494cdcc59a99460626b0ab5aa201abb93283e05f
cheng <928720204@qq.com>
2024-02-29 21:21:40 +0800

    first commit

---
+++ src/main/java/com/example/oauth2logindemo/demos/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.example.oauth2logindemo.demos.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author Cheng
+ * @Date 2024 02 28 22 53
+ **/
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                        @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());
+        return "index";
+    }
+}
